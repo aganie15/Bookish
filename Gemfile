961205424db77522dd9b6028870117a0350e9a34
aganie15 <aga.niewiadomska@softwire.com>
2022-09-21 17:26:04 +0100

    Finished styling

--- Gemfile
+++ Gemfile
@@ -48,6 +48,13 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+# Use Bulma for styling
+gem "bulma-rails", "~> 0.9.3"
+gem "bulma-extensions-rails"
+
+# Use plugin to validate dates
+gem 'date_validator', '~> 0.12.0'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -1,29 +1,30 @@
 class CopiesController < ApplicationController
+  before_action :find_book
   def create
-    @book = Book.find(params[:book_id])
     @copy = @book.copies.create(copy_params)
     redirect_to book_path(@book)
   end
 
+  def new
+    @copy = Copy.new
+  end
   def destroy
-    @book = Book.find(params[:book_id])
     @copy = @book.copies.find(params[:id])
     @copy.destroy
     redirect_to book_path(@book), status: :see_other
   end
 
   def edit
-    @book = Book.find(params[:book_id])
     @copy = @book.copies.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:book_id])
     @copy = @book.copies.find(params[:id])
 
     if @copy.update(copy_params)
       redirect_to book_path(@book)
     else
+      puts "Invalid input error"
       render :edit, status: :unprocessable_entity
     end
   end
@@ -31,5 +32,9 @@ class CopiesController < ApplicationController
   private
   def copy_params
     params.require(:copy).permit(:borrower, :due_date)
+  end
+
+  def find_book
+    @book = Book.find(params[:book_id])
   end
 end
